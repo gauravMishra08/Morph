9e2f79928661f8640c7e8f143b610ba5dda76d83
Utakarsh Jain <utakarshjain2@gmail.com>
2025-04-11 17:50:41 +0530

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,7 +5,7 @@ export const Footer: React.FC = () => {
     <footer className="mt-16 px-6 sm:px-12 md:px-16 py-8 bg-[#101217] text-white">
       <div className="border-t border-[#FF4B39]" />
       <div className="mt-6 text-center text-sm">
-        Designed and Developed by Team A101 🫂
+        Designed and Developed by Team A101
       </div>
     </footer>
   );
